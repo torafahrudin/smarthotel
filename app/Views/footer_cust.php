8200230c21697cf0aaea1ef80820adc7677b863b
sofyanhadi <sofyanhady81@gmail.com>
2021-12-23 09:43:02 +0700

    menginstal template

---
+++ app/Views/footer_cust.php
@@ -0,0 +1,259 @@
+<div class="footer-top section-padding bg-black">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2 col-sm-4 col-6 mb-sm-20">
+                    <div class="icon-box"> <span class="text-light-green"><i class="flaticon-credit-card-1"></i></span>
+                        <span class="text-custom-white">100% Payment<br>Secured</span>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6 mb-sm-20">
+                    <div class="icon-box"> <span class="text-light-green"><i class="flaticon-wallet-1"></i></span>
+                        <span class="text-custom-white">Support lots<br> of Payments</span>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6 mb-sm-20">
+                    <div class="icon-box"> <span class="text-light-green"><i class="flaticon-help"></i></span>
+                        <span class="text-custom-white">24 hours / 7 days<br>Support</span>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <div class="icon-box"> <span class="text-light-green"><i class="flaticon-truck"></i></span>
+                        <span class="text-custom-white">Free Delivery<br>with $50</span>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <div class="icon-box"> <span class="text-light-green"><i class="flaticon-guarantee"></i></span>
+                        <span class="text-custom-white">Best Price<br>Guaranteed</span>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <div class="icon-box"> <span class="text-light-green"><i class="flaticon-app-file-symbol"></i></span>
+                        <span class="text-custom-white">Mobile Apps<br>Ready</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="section-padding bg-light-theme pt-0 u-line bg-black">
+        <div class="u-line instagram-slider swiper-container">
+            <ul class="hm-list hm-instagram swiper-wrapper">
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-3.jpg" alt="instagram"></a>
+                </li>
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-1.jpg" alt="instagram"></a>
+                </li>
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-2.jpg" alt="instagram"></a>
+                </li>
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-4.jpg" alt="instagram"></a>
+                </li>
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-5.jpg" alt="instagram"></a>
+                </li>
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-6.jpg" alt="instagram"></a>
+                </li>
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-7.jpg" alt="instagram"></a>
+                </li>
+                <li class="swiper-slide">
+                    <a href="#"><img src="<?php echo base_url('') ?>/assetcust/img/restaurants/250x200/insta-8.jpg" alt="instagram"></a>
+                </li>
+            </ul>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl col-lg-4 col-md-4 col-sm-6">
+                    <div class="footer-contact">
+                        <h6 class="text-custom-white">Need Help</h6>
+                        <ul>
+                            <li class="fw-600"><span class="text-light-white">Call Us</span> <a href="tel:" class="text-custom-white">+(347) 123 456 789</a>
+                            </li>
+                            <li class="fw-600"><span class="text-light-white">Email Us</span> <a href="mailto:" class="text-custom-white">demo@domain.com</a>
+                            </li>
+                            <li class="fw-600"><span class="text-light-white">Join our twitter</span> <a href="#" class="text-custom-white">@munchbox</a>
+                            </li>
+                            <li class="fw-600"><span class="text-light-white">Join our instagram</span> <a href="#" class="text-custom-white">@munchbox</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl col-lg-4 col-md-4 col-sm-6">
+                    <div class="footer-links">
+                        <h6 class="text-custom-white">Get to Know Us</h6>
+                        <ul>
+                            <li><a href="#" class="text-light-white fw-600">About Us</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Blog</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Socialize</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Munchbox</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Perks</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl col-lg-4 col-md-4 col-sm-6">
+                    <div class="footer-links">
+                        <h6 class="text-custom-white">Let Us Help You</h6>
+                        <ul>
+                            <li><a href="#" class="text-light-white fw-600">Account Details</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Order History</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Find restaurant</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Login</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Track order</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl col-lg-4 col-md-4 col-sm-6">
+                    <div class="footer-links">
+                        <h6 class="text-custom-white">Doing Business</h6>
+                        <ul>
+                            <li><a href="#" class="text-light-white fw-600">Suggest an Idea</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Be a Partner restaurant</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Create an Account</a>
+                            </li>
+                            <li><a href="#" class="text-light-white fw-600">Help</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl col-lg-4 col-md-4 col-sm-6">
+                    <div class="footer-links">
+                        <h6 class="text-custom-white">Download Apps</h6>
+                        <div class="appimg">
+                            <a href="#">
+                                <img src="<?php echo base_url('') ?>/assetcust/img/playstore.jpg" class="img-fluid" alt="app logo">
+                            </a>
+                        </div>
+                        <div class="appimg">
+                            <a href="#">
+                                <img src="<?php echo base_url('') ?>/assetcust/img/appstore.jpg" class="img-fluid" alt="app logo">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl col-lg-4 col-md-4 col-sm-6">
+                    <div class="footer-contact">
+                        <h6 class="text-custom-white">Newsletter</h6>
+                        <form class="subscribe_form">
+                            <div class="input-group">
+                                <input type="text" class="form-control form-control-submit" name="email" placeholder="Enter your email">
+                                <span class="input-group-btn">
+                      <button class="btn btn-second btn-submit" type="button"><i class="fas fa-paper-plane"></i></button>
+                 </span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="ft-social-media">
+                        <h6 class="text-center text-light-black">Follow us</h6>
+                        <ul>
+                            <li> <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-instagram"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                            </li>
+                            <li> <a href="#"><i class="fab fa-youtube"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="copyright bg-black">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="payment-logo mb-md-20"> <span class="text-light-white fs-14 mr-3">We are accept</span>
+                        <div class="payemt-icon">
+                            <img src="<?php echo base_url('') ?>/assetcust/img/card-front.jpg" alt="#">
+                            <img src="<?php echo base_url('') ?>/assetcust/img/visa.jpg" alt="#">
+                            <img src="<?php echo base_url('') ?>/assetcust/img/amex-card-front.png" alt="#">
+                            <img src="<?php echo base_url('') ?>/assetcust/img/mastercard.png" alt="#">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 text-center medewithlove align-self-center">
+                    
+                </div>
+                <div class="col-lg-4">
+                    <div class="copyright-text"> <span class="text-light-white"><a href="templateshub.net">Templates Hub</a> </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <!-- modal boxes -->
+    <div class="modal fade" id="address-box">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title fw-700">Change Address</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="location-picker">
+                        <input type="text" class="form-control" placeholder="Enter a new address">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="search-box">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <div class="search-box p-relative full-width">
+                        <input type="text" class="form-control" placeholder="Pizza, Burger, Chinese">
+                    </div>
+                </div>
+                <div class="modal-body"></div>
+            </div>
+        </div>
+    </div>
+    <!-- Place all Scripts Here -->
+    <!-- jQuery -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/jquery.min.js"></script>
+    <!-- Popper -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/popper.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/bootstrap.min.js"></script>
+    <!-- Range Slider -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/ion.rangeSlider.min.js"></script>
+    <!-- Swiper Slider -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/swiper.min.js"></script>
+    <!-- Nice Select -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/jquery.nice-select.min.js"></script>
+    <!-- magnific popup -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/jquery.magnific-popup.min.js"></script>
+    <!-- Maps -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDnd9JwZvXty-1gHZihMoFhJtCXmHfeRQg"></script>
+    <!-- sticky sidebar -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/sticksy.js"></script>
+    <!-- Munch Box Js -->
+    <script src="<?php echo base_url('') ?>/assetcust/js/munchbox.js"></script>
+    <!-- /Place all Scripts Here -->
+</body>
+
+
+<!-- munchbox/index.html  05 Dec 2019 10:13:26 GMT -->
+</html>
---
+++ app/Views/templates/menu.php
@@ -0,0 +1,175 @@
+<div class="left-side-menu">
+
+    <div class="slimscroll-menu">
+
+        <!-- User box -->
+        <div class="user-box text-center">
+            <img src="<?= base_url(); ?>/assets/images/users/user-1.jpg" alt="user-img" title="Mat Helme" class="rounded-circle img-thumbnail avatar-md">
+            <div class="dropdown">
+                <a href="#" class="user-name dropdown-toggle h5 mt-2 mb-1 d-block" data-toggle="dropdown" aria-expanded="false">username</a>
+            </div>
+            <!-- <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a href="#" class="text-muted">
+                        <i class="mdi mdi-cog"></i>
+                    </a>
+                </li>
+
+                <li class="list-inline-item">
+                    <a href="#">
+                        <i class="mdi mdi-power"></i>
+                    </a>
+                </li>
+            </ul> -->
+        </div>
+
+        <!--- Sidemenu -->
+
+        <!--- Sidemenu hrd-->
+       
+            <div id="sidebar-menu">
+
+                <ul class="metismenu" id="side-menu">
+
+                    <li class="menu-title">Home</li>
+
+                    <li>
+                        <a href="/">
+                            <i class="mdi mdi-view-dashboard"></i>
+                            <span> Dashboard </span>
+                        </a>
+                    </li>
+
+
+                    <li class="menu-title">Master Data</li>
+
+                    <!-- <li>
+                        <a href="/coa">
+                            <i class="mdi mdi-forum"></i>
+                            <span> COA </span>
+                        </a>
+                    </li> -->
+
+<!-- 
+                    <li>
+                        <a href="/pegawaitetap">
+                            <i class="mdi mdi-account-group"></i>
+                            <span> Pegawai</span>
+                        </a>
+                    </li> -->
+
+                    <li>
+                        <a href="/jabatan">
+                            <i class="mdi mdi-ballot"></i>
+                            <span> Data Penjualan</span>
+                        </a>
+                    </li>
+
+                    <!-- <li>
+                        <a href="/tunjangan">
+                            <i class="mdi mdi-briefcase-account-outline"></i>
+                            <span> Tunjangan </span>
+                        </a>
+                    </li> -->
+                    <!-- <li>
+                        <a href="/ptkp">
+                            <i class="mdi mdi-briefcase-account-outline"></i>
+                            <span> PTKP </span>
+                        </a>
+                    </li> -->
+                    <!-- <li>
+                        <a href="/service_charge">
+                            <i class="mdi mdi-broom"></i>
+                            <span> Service Charge </span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="/users">
+                            <i class="mdi mdi-account-cog"></i>
+                            <span> Users Akses</span>
+                        </a>
+                    </li>
+ -->
+                    <li class="menu-title">Transaction</li>
+
+                  <!--   <li>
+                        <a href="/kehadiran">
+                            <i class="mdi mdi-presentation"></i>
+                            <span> Kehadiran </span>
+                        </a>
+                    </li -->>
+                    <li class="menu-title">Sistem Rekomendasi</li>
+
+                    <li>
+                        <a href="<?php echo base_url('Asociationrule') ?>">
+                            <i class="mdi mdi-presentation"></i>
+                            <span> Metode Asosiasi</span>
+                        </a>
+                    </li>
+                    <!-- <li class="menu-title">Transaction</li>
+
+                    <li>
+                        <a href="/kehadiran">
+                            <i class="mdi mdi-presentation"></i>
+                            <span> Kehadiran </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/penggajian">
+                            <i class="mdi mdi-account-tie-voice"></i>
+                            <span> Penggajian </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/transaksi_service_charge">
+                            <i class="mdi mdi-room-service"></i>
+                            <span> Service Charge </span>
+                        </a>
+                    </li>
+
+                    <li class="menu-title">Report</li>
+
+                    <li>
+                        <a href="/laporan/kehadiran">
+                            <i class="mdi mdi-file-account"></i>
+                            <span> Laporan Kehadiran </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/laporan/penggajian">
+                            <i class="mdi mdi-file-account"></i>
+                            <span> Laporan Penggajian </span>
+                        </a>
+                    </li> -->
+
+                    <!-- <li>
+                        <a href="/jurnal">
+                            <i class="mdi mdi-clipboard-file"></i>
+                            <span> Jurnal Umum </span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="/buku_besar">
+                            <i class="mdi mdi-file-document"></i>
+                            <span> Buku Besar </span>
+                        </a>
+                    </li> -->
+
+                </ul>
+
+            </div>
+    
+
+       
+
+
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
