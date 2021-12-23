8deb3dd60d487060c41d635b96bfcbb76c06ba1d
sofyanhadi <sofyanhady81@gmail.com>
2021-12-23 23:35:24 +0700

    edit base controler helpr

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ class BaseController extends Controller
      *
      * @var array
      */
-    protected $helpers = ['html','form','url','filesystem','rupiah'];
+    protected $helpers = ['html','form','url','filesystem','Rupiah'];
 
     /**
      * Constructor.
