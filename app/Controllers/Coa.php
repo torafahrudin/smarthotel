d650b8b320c2de65760eb1eaf3f680a659b25040
bbjkonaw <konah359@gmail.com>
2021-12-23 16:48:45 +0700

    controller coa, home

--- app/Controllers/Coa.php
+++ app/Controllers/Coa.php
@@ -1,23 +1,61 @@
 <?php
+
 namespace App\Controllers;
 
-use App\Models\CoaModel;
+use \App\Models\CoaModel;
 
 class Coa extends BaseController
 {
+    protected $coaModel;
+
     public function __construct()
     {
-		session_start();
-        $this->CoaModel = new CoaModel();
+        $this->coaModel = new CoaModel();
     }
 
-	public function index()
-	{
+    public function index()
+    {
+        $data = [
+            'title'     => 'Data COA',
+            'coa'       => $this->coaModel->getCoa(),
+        ];
+        return view('coa/view_data_coa', $data);
+    }
 
-        $data['coa'] = $this->CoaModel->getAll();
+    public function add()
+    {
+        $data = array(
+            'id_akun' => $this->request->getPost('id_akun'),
+            'nama_akun' => $this->request->getPost('nama_akun'),
+            'kategori' => $this->request->getPost('kategori'),
+            'saldo_normal' => $this->request->getPost('saldo_normal'),
+        );
+        $this->coaModel->createCoa($data);
+        session()->setFlashdata('success', 'Data COA Berhasil Ditambahkan');
 
-        print_r($data['coa']);
-        echo "<br>";
-        echo "perubahan pertama";
-	}
+        return redirect()->to('/coa');
+    }
+
+    public function edit()
+    {
+        $id = $this->request->getPost('id_akun');
+        $data = array(
+            'nama_akun' => $this->request->getPost('nama_akun'),
+            'kategori' => $this->request->getPost('kategori'),
+            'saldo_normal' => $this->request->getPost('saldo_normal'),
+        );
+        $this->coaModel->updateCoa($data, $id);
+        session()->setFlashdata('success', 'Data COA Berhasil Diubah');
+
+        return redirect()->to('/coa');
+    }
+
+    public function delete()
+    {
+        $id = $this->request->getPost('id_akun');
+        $this->coaModel->deleteCoa($id);
+        session()->setFlashdata('success', 'Data COA Berhasil Dihapus');
+
+        return redirect()->to('/coa');
+    }
 }
