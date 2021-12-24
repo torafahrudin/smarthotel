5f835e2030debb0dae2606a6ca08c1c6580a303a
egitanf <egitanurfadila@student.telkomuniversity.ac.id>
2021-12-24 15:48:44 +0700

    egita edit top bar

--- app/Views/templates/topbar.php
+++ app/Views/templates/topbar.php
@@ -21,7 +21,7 @@
             <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                 <img src="<?= base_url(); ?>/assets/images/users/user-1.jpg" alt="user-image" class="rounded-circle">
                 <span class="pro-user-name ml-1">
-                    <?= user()->username; ?> <i class="mdi mdi-chevron-down"></i>
+                    <i class="mdi mdi-chevron-down"></i>
                 </span>
             </a>
             <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
