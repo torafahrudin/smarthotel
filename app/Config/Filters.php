e13586c6df379568bfa69487e89f79731cc591a5
mellymeil <mellymeilawati95@gmail.com>
2021-12-24 21:41:51 +0700

    update auth

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -19,6 +19,9 @@ class Filters extends BaseConfig
         'csrf'     => CSRF::class,
         'toolbar'  => DebugToolbar::class,
         'honeypot' => Honeypot::class,
+        'login'      => \Myth\Auth\Filters\LoginFilter::class,
+        'role'       => \Myth\Auth\Filters\RoleFilter::class,
+        'permission' => \Myth\Auth\Filters\PermissionFilter::class,
     ];
 
     /**
@@ -29,8 +32,9 @@ class Filters extends BaseConfig
      */
     public $globals = [
         'before' => [
-            // 'honeypot',
+            'honeypot',
             // 'csrf',
+            'login',
         ],
         'after' => [
             'toolbar',
@@ -58,5 +62,7 @@ class Filters extends BaseConfig
      *
      * @var array
      */
-    public $filters = [];
+    public $filters = [
+        // 'login' => ['before' => ['admin/*']]
+    ];
 }
